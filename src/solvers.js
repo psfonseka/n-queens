556227dce01bf79a1600cd982f32bfba222c1112
psfonseka <psfonseka@gmail.com>
2019-07-24 16:56:58 -0400

    Turns out the logic for nrookssolutions works for queens too

--- src/solvers.js
+++ src/solvers.js
@@ -52,7 +52,7 @@ window.countNRooksSolutions = function(n) {
         helper(size,curRow+1,nBoard);
       }
     }
-  }
+  };
   helper(n,0,solution);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -60,15 +60,36 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({'n': n});
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var solution = new Board({'n': n});
+  console.log('original', solution.rows());
+  var helper = function(size, curRow, board) {
+    if (curRow === size) { //When "current row" is equal to the size, the board is filled
+      if (!board.hasAnyQueensConflicts()) {
+        solutionCount++;
+      }
+    } else if (!board.hasAnyQueensConflicts()){
+      for (var i = 0; i < size; i++) {
+        boardMatrix = board.rows();
+        newMatrix = [];
+        for (var k = 0; k < boardMatrix.length; k++) {
+          newMatrix.push(boardMatrix[k].slice());
+        }
+        nBoard = new Board(newMatrix);
+        nBoard.togglePiece(curRow,i);
+        helper(size,curRow+1,nBoard);
+      }
+    }
+  };
+  helper(n,0,solution);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
