c91e48e9793b2d895df007bdcb0184cddb6be81d
psfonseka <psfonseka@gmail.com>
2019-07-25 15:34:20 -0400

    Implemented bitwise solution

--- src/Bitwise.js
+++ src/Bitwise.js
@@ -1,7 +1,7 @@
 //Going to attempt the bitwise solution (or go as far as i possible can with this)
 find8Queens = () => {
   var solutions = 0;
-  var binArr = [128,64,32,16,8,4,2,1]
+  var binArr = [128, 64, 32, 16, 8, 4, 2, 1];
   var startCol = 0 << 1;
   var leftDiag = 0 >> 1;
   var rightDiag = 0 << 1;
@@ -9,28 +9,59 @@ find8Queens = () => {
 
   var visualizeBinary = (n) => {
     console.log(("00000000"+n.toString(2)).slice(-8));
-  }
+  };
 
   var helper = (col, ld, rd, row, q) => {
     col = col + q;  //processing everything with the addition of the new queen
     ld = (ld + q) << 1;
+    if (ld > 255) {
+      ld = ld - 256;
+    }
     rd = (rd + q) >> 1;
-    var remaining = (col | ld | rd); //Bitwise combine all attacks to check conflicts
-    if(row === maxIndex) { //Base Case
-      if (remaining < 255) { //If all bits = 1, then 255 is what it equals total(meaning no remaining place for final queen)
+    var attacking = (col | ld | rd); //Bitwise combine all attacks to check conflicts
+    if (row === maxIndex) { //Base Case
+      if (attacking < 255) { //If all bits = 1, then 255 is what it equals total(meaning no remaining place for final queen)
         solutions++;
       }
     } else {
-      console.log(255-remaining);
+      var remaining = 255-attacking;
+      //console.log(remaining);
+      while(remaining > 0) {
+        if (remaining >= 128) {
+          remaining = remaining - 128;
+          helper(col,ld,rd,row+1,128);
+        } else if (remaining >= 64) {
+          remaining = remaining - 64;
+          helper(col,ld,rd,row+1,64);
+        } else if (remaining >= 32) {
+          remaining = remaining - 32;
+          helper(col,ld,rd,row+1,32);
+        }  else if (remaining >= 16) {
+          remaining = remaining - 16;
+          helper(col,ld,rd,row+1,16);
+        }  else if (remaining >= 8) {
+          remaining = remaining - 8;
+          helper(col,ld,rd,row+1,8);
+        }  else if (remaining >= 4) {
+          remaining = remaining - 4;
+          helper(col,ld,rd,row+1,4);
+        }  else if (remaining >= 2) {
+          remaining = remaining - 2;
+          helper(col,ld,rd,row+1,2);
+        } else {
+          remaining = remaining - 1;
+          helper(col,ld,rd,row+1,1);
+        }
+      }
+      //helper(col,ld,rd,row+1,q);
     }
-    console.log(col);
-    console.log(ld);
-    console.log(rd);
-    console.log(row);
-  }
+    //console.log(col, ld, rd, row);
+  };
 
   helper(startCol, leftDiag, rightDiag, 0, 0); //everything starts equal to 0
   //visualizeBinary(235);
-}
+  return solutions;
+};
 
 var result = find8Queens();
+console.log(result);
